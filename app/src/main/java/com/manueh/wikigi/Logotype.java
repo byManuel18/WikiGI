56e0873310143eccc771dc7092dc08d2199dbecb
Manuel <antoniomanu533@gmail.com>
2020-10-29 14:11:39 +0100

    Initial commit

---
+++ app/src/main/java/com/manueh/wikigi/Logotype.java
@@ -0,0 +1,27 @@
+package com.manueh.wikigi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Logotype extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logotype);
+        getSupportActionBar().hide();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
