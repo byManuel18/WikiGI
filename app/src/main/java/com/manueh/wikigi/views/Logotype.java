f657aa5bcbd403f7b92853e998b80afbbb3048a4
Manuel <antoniomanu533@gmail.com>
2020-11-12 14:25:08 +0100

    Lista Personajes completada. Botón flecha formulario.

--- app/src/main/java/com/manueh/wikigi/views/Logotype.java
+++ app/src/main/java/com/manueh/wikigi/views/Logotype.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.manueh.wikigi.R;
 
@@ -11,12 +12,14 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Logotype extends AppCompatActivity {
-
+    private String TAG="wikigi/Logotype";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logotype);
         getSupportActionBar().hide();
+        Log.d(TAG,"Método que quita el logo después de 3 segundos");
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
